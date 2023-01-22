847d08be1636aea8800526cbbaa4f1ee355d5693
alpha2207_dev <ayan787ali.mev@gmail.com>
2023-01-22 08:49:26 +0530

    Added required functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
 function App() {
+  const [waid, setWaid] = useState('');
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    console.log(searchParams);
+    const waId = searchParams.get('waId');
+    if (waId) {
+      setWaid(waId);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      {waid && <p>waid</p>}
+      <button onClick={() => window.location.href = "https://sanjana.authlink.me/?redirectUri=http%3A%2F%2Flocalhost%3A3000%2F"}>Click Me</button>
+    </div >
   );
 }
 
